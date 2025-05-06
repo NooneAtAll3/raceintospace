c82f79c95617fb14dc58e7547ab173c5c9de4649
NooneAtAll3 <136939641+NooneAtAll3@users.noreply.github.com>
2025-05-06 02:18:39 +0000

    Update crash.cpp

--- src/game/crash.cpp
+++ src/game/crash.cpp
@@ -102,7 +102,7 @@ void pretty_crash(const std::string &title, const std::string &message)
     while (!(key == K_ENTER || key == K_ESCAPE || key == K_SPACE)) {
         av_block();
         gr_sync();
-        key = pressed_key();
+        key = get_pressed_key();
     }
 
     // exit with an error code
